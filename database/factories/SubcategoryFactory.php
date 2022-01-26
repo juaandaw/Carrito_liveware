a0608bcaa2f4bb29fca5da5a71a6246e5245e25e
juaandaw <juantejadamartorell5@gmail.com>
2022-01-26 16:01:08 +0000

    Proyecto ecommerce desde el punto 5

---
+++ database/factories/SubcategoryFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SubcategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'image' => 'subcategproes/'.$this->faker->image(storage_path('app/public/subcategories'),640,480,null,false)
+        ];
+    }
+}
