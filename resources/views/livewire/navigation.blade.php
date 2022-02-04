267965ee777933ec8eec15c20427b27987259b53
juaandaw <juantejadamartorell5@gmail.com>
2022-02-04 19:28:56 +0000

    Fin pdf 6 completado

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,19 +1,11 @@
-<style>
-    #navigation-menu{
-        height: calc(100vh - 4rem);
-    }
-    .navigation-link:hover .navigation-submenu{
-        display: block !important;
-    }
-</style>
-<header class="bg-trueGray-700 sticky top-0">
+<header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
     <div class="container-menu flex items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a :class="{bg-opacity-100 text-orange-500: open}" x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4
 12h16M4 18h16" />
             </svg>
-            <span>
+            <span class="text-sm">
                 Categorias
             </span>
             <a href="/" class="mx-6">
@@ -75,9 +67,9 @@
         </div>
         @livewire('dropdown-cart')
     </div>
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu" x-show="open" :class="{'block': open, 'hidden': !open}" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
         <div class="container-menu h-full">
-            <div class="grid grid-cols-4 h-full">
+            <div x-on:click.away="close()" class="grid grid-cols-4 h-full">
                 <ul class="bg-white">
                     @foreach($categories as $category)
                         <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,1 @@
-<x-app-layout>
-    <x-alert type="error" position="top-right">Esta es mi alerta</x-alert>
-</x-app-layout>
+<x-app-layout></x-app-layout>
