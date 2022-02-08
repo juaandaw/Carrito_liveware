f636178851f90a9947b53c8e3eac692feac65ef2
juaandaw <juantejadamartorell5@gmail.com>
2022-02-08 21:32:52 +0000

    Fin paso 37 pdf 9 apartado de categorias que muestran los productos de sus subcategorias

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,8 +15,8 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory(50)->create()->each(function(Product $product){
-            Image::factory(4)->create([
+        Product::factory(250)->create()->each(function(Product $product){
+            Image::factory(8)->create([
                 'imageable_id' => $product->id,
                 'imageable_type' => Product::class
             ]);
