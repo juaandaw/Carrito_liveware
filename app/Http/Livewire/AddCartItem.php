a159cea976b767cf0b9c022388301e0ef55f5765
juaandaw <juantejadamartorell5@gmail.com>
2022-02-09 16:37:16 +0000

    Fin paso 48 ponemos limites al stock del producto

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -6,7 +6,15 @@ use Livewire\Component;
 
 class AddCartItem extends Component
 {
+    public $product;
+    public $quantity;
     public $qty = 1;
+
+    public function mount() //ciclo de vida se ejecuta al abrir la pagina load c#
+    {
+        $this->quantity = $this->product->quantity;
+    }
+
     public function render()
     {
         return view('livewire.add-cart-item');
