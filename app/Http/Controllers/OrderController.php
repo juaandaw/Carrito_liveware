6759d38449fd83545fa4d7e058a04b0d541d29e9
juaandaw <juantejadamartorell5@gmail.com>
2022-02-15 09:15:28 +0000

    Fin video 110 orden de pago finalizada

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,19 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function payment(Order $order)
-    {
-        $items = json_decode($order->content);
-
-        return view('orders.payment',compact('order','items'));
-    }
-
     public function show(Order $order)
     {
-        return view('orders.show',compact('order'));
+        $items = Cart::content();
+        return view('orders.show',compact('order','items'));
     }
 }
