e1b9bf7f0938150e8b3b3c7927129c13d4479147
juaandaw <juantejadamartorell5@gmail.com>
2022-02-15 10:30:17 +0000

    Fin paso 112 enlace mis ordenes y traducción al español

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+
+    public function index()
+    {
+        return view('orders.index');
+    }
     public function show(Order $order)
     {
         $this->authorize('view',$order);
