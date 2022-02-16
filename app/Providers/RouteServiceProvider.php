4475fe74e64f9b30d473ff37b457a132ce6ecbfb
juaandaw <juantejadamartorell5@gmail.com>
2022-02-16 17:00:30 +0000

    Fin paso 121 vista de administracion

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::middleware(['web','auth'])
+            ->prefix('admin')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
     }
 
     /**
