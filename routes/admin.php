84f714ed28b57e316b33895ac48efa31859134ec
juaandaw <juantejadamartorell5@gmail.com>
2022-02-16 19:15:44 +0000

    Fin paso 125 selects de categorias y subcategorias en creacion

--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,5 @@ use App\Http\Livewire\Admin\ShowProducts;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
-Route::get('products/{product}/edit',function (){})->name('admin.products.edit');
+Route::get('products/create',\App\Http\Livewire\Admin\CreateProduct::class)->name('admin.products.create');
 Route::get('products/{product}/edit',function (){})->name('admin.products.edit');
