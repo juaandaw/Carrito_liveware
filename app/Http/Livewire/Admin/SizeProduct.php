50ab57c373b769b5d76e7129e125eb5d83984c18
juaandaw <juantejadamartorell5@gmail.com>
2022-02-18 16:53:20 +0000

    Fin paso 143 modal del nombre de la talla acabado

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Size;
 use Livewire\Component;
 
 class SizeProduct extends Component
 {
     public $product,$name;
+    public $open = false;
+    public $name_edit;
     public $rules = [
         'name' => 'required'
     ];
@@ -20,6 +23,27 @@ class SizeProduct extends Component
         ]);
 
         $this->product = $this->product->fresh();
+    }
+
+    public function edit(Size $size)
+    {
+        $this->open = true;
+        $this->size = $size;
+        $this->name_edit = $size->name;
+
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'name_edit' => 'required'
+        ]);
+        $this->size->name = $this->name_edit;
+        $this->size->save();
+
+        $this->product = $this->product->fresh();
+
+        $this->open = false;
     }
     public function render()
     {
