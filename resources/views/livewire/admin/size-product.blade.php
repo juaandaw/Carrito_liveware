13696b3627ca8e4e6756c8ba736483a75efc4d46
juaandaw <juantejadamartorell5@gmail.com>
2022-02-18 17:30:21 +0000

    Fin paso 145 agregar colores y resumen del color y su cantidad en  nuestro producto con talla y colores

--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -34,6 +34,7 @@
                         </x-jet-danger-button>
                     </div>
                 </div>
+                @livewire('admin.color-size',['size' => $size],key('color-size-',$size->id))
             </li>
         @endforeach
     </ul>
