b3e239fc9a5720a99ca8cc0116975b5a578fadd8
juaandaw <juantejadamartorell5@gmail.com>
2022-02-18 16:36:37 +0000

    Fin paso 142 creacion de tallas vista edit

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -6,8 +6,24 @@ use Livewire\Component;
 
 class SizeProduct extends Component
 {
+    public $product,$name;
+    public $rules = [
+        'name' => 'required'
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->product->sizes()->create([
+            'name' => $this->name,
+        ]);
+
+        $this->product = $this->product->fresh();
+    }
     public function render()
     {
-        return view('livewire.admin.size-product');
+        $sizes = $this->product->sizes; // me traigo todos los registros de la relacion talla con este producto
+        return view('livewire.admin.size-product',compact('sizes'));
     }
 }
