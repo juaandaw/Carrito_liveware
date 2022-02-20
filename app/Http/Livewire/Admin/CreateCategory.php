e1837f713bd268032e8503c38aef81ddf0477d3c
juaandaw <juantejadamartorell5@gmail.com>
2022-02-20 09:51:58 +0000

    Fin paso 159 le damos un nombre mas identificativo a los campos que dan error gracias al validationAttribute

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -3,16 +3,47 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateCategory extends Component
 {
+    use WithFileUploads;
 
     public $brands;
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required'
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
 
     public function mount()
     {
         $this->getBrands();
+    }
+
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
     }
 
     public function getBrands()
@@ -21,7 +52,7 @@ class CreateCategory extends Component
     }
     public function save()
     {
-
+        $this->validate();
     }
     public function render()
     {
