048963e723bc9523cc642816c9de9dc8815a4d57
juaandaw <juantejadamartorell5@gmail.com>
2022-02-20 13:02:16 +0000

    Fin paso 163 boton para editar terminado

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -12,9 +12,18 @@ class CreateCategory extends Component
 {
     use WithFileUploads;
 
-    public $brands,$categories,$image;
+    public $brands,$categories,$image,$category,$editImage;
     protected $listeners = ['delete'];
     public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    public $editForm = [
+        'open' => false,
         'name' => null,
         'slug' => null,
         'icon' => null,
@@ -78,6 +87,17 @@ class CreateCategory extends Component
 
         $this->getCategories();
         $this->emit('saved');
+    }
+
+    public function edit(Category $category)
+    {
+        $this->category = $category;
+        $this->editForm['open'] = true;
+        $this->editForm['name'] = $category->name;
+        $this->editForm['slug'] = $category->slug;
+        $this->editForm['icon'] = $category->icon;
+        $this->editForm['image'] = $category->image;
+        $this->editForm['brands'] = $category->brands->pluck('id');
     }
 
     public function delete(Category $category)
