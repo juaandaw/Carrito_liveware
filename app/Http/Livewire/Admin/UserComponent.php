580e2dead822d74be2d22b5c0598b76b9fa41be5
juaandaw <juantejadamartorell5@gmail.com>
2022-02-21 21:10:17 +0000

    Fin paso 184 tambien filtramos por email

--- app/Http/Livewire/Admin/UserComponent.php
+++ app/Http/Livewire/Admin/UserComponent.php
@@ -4,12 +4,21 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UserComponent extends Component
 {
+    use WithPagination;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
     public function render()
     {
-        $users = User::paginate();
+        $users = User::where('name', 'LIKE', "%{$this->search}%")
+            ->orWhere('email', 'LIKE', "%{$this->search}%")
+            ->paginate();
         return view('livewire.admin.user-component',compact('users'))->layout('layouts.admin');
     }
 }
